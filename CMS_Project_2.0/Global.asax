8d20f23b6856c37244ebae0aed6d006c59605e8c
tamilselvan719 <tamilselvan719@hotmail.com>
2017-04-05 18:09:47 +0530

    Day1: Tutorial Completed

---
+++ CMS_Project_2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CMS_Project_2._0.Global" Language="C#" %>
---
+++ DataModel/CMS_Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace DataModel
+{
+    public class CMS_Context : DbContext
+    {
+        public CMS_Context()
+            : base("name=CMS_Context")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+    }
+}
